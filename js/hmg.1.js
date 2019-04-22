967e842cc522db260b6a61b61e5c0e230d6949ef
aakash819 <aakash.goel462@gmail.com>
2019-04-22 14:46:20 +0530

    change script name  for hmg.html

--- js/hmg.1.js
+++ js/hmg.1.js
@@ -1,5 +1,6 @@
 var uniqueID = "";
 
+
 (function () {
     var d = new Date();
     var date = d.getDate();
@@ -32,6 +33,7 @@ var uniqueID = "";
 
     regForm.addEventListener('onValidationSuccess', function () {
 
+
         var paymentType = $("input[name='payment_type']:checked").val();
 
         var price = $("input[name='price']:checked").val();
@@ -60,9 +62,8 @@ var uniqueID = "";
         };
 
         if (Captha()) {
-            $("#loader").show();
-
             $('#fieldForm').prop('disabled', true);
+            $("#loader").show();
             $.post({
                 'url': "https://mojogamezone.com/development/riccha_dev/HairProduct/setFormData.php",
                 'data': JSON.stringify(dataToSend),
@@ -73,10 +74,12 @@ var uniqueID = "";
                         pymentSuccess(parse_data.data);
                     }
                     $('#order-msg').show();
+
                 },
                 'complete': function () {
                     $('#fieldForm').prop('disabled', false);
                     regForm.reset();
+                    regForm.clearValidation();
                     grecaptcha.reset();
 
                 }
@@ -90,7 +93,7 @@ var uniqueID = "";
             $('#order').modal('show');
         }
     });
-    $(' #order-msg').hide();
+    $('#order-msg').hide();
 })();
 
 function pymentSuccess(id) {
@@ -105,7 +108,10 @@ function pymentSuccess(id) {
         'success': function (res, status) {
             var response = JSON.parse(res);
             $('#order-msg').hide();
+
+
             if (response.data.payment_type == 'COD') {
+         
                 $("#ModalTitle").html('Order details');
                 $('#ord-msg').html('Thank You for Placing your order, your order ID is' + '  "' + response.data.order_id + '" , we have also sent a mail to your mail ID.' + ' ');
                 $('#ord-msg , #ModalTitle').addClass('color-dark-green').removeClass('color-red');
@@ -117,14 +123,19 @@ function pymentSuccess(id) {
                 $("#TXN_AMOUNT").val(response.data.price);
                 document.hmg.submit();
             }
-          gtag_report_conversion();
+            gtag_report_conversion();
         },
         'complete': function () {
             $("#loader").hide();
             $('#after_otp,#otp,#checkOTP').prop('disabled', true);
+
         }
     });
 }
+
+
+
+
 
 function gtag_report_conversion(url) {
     var callback = function () {
@@ -139,37 +150,98 @@ function gtag_report_conversion(url) {
     return false;
 }
 
+
 $('#sendOTP').click(function () {
-    $('#sendOTP').prop('disabled', true);
-    setTimeout(() => {
+
+    var phoneInput = document.getElementById('phone');
+    validateField(phoneInput);
+
+    if (!phoneInput.classList.contains('is-valid')) {
+        return;
+    }
+
+    togglePhoneField(true);
+    toggleSendOtpProgressBar(true);
+    // $('#sendOTP').prop('disabled', true);
+    /* setTimeout(() => {
         $('#sendOTP').prop('disabled', false);
-    }, 20000);
-    var phoneNumber = $("#phone").val();
+    }, 20000); */
+    /* var phoneNumber = $("#phone").val();
     var length = phoneNumber.length;
     if (length >= 10 && length <= 16) {
-        if (isTel(phoneNumber)) {
-            var dataToSend = {
+        if (isTel(phoneNumber)) { */
+    var dataToSend = {
 
-                "firstName": $('#fname').val(),
-                "lastName": $('#lname').val(),
-                "email": $('#email').val(),
-                "phone": $('#phone').val(),
+        "firstName": $('#fname').val(),
+        "lastName": $('#lname').val(),
+        "email": $('#email').val(),
+        "phone": $('#phone').val(),
 
-            };
-            $.post({
-                'url': "https://mojogamezone.com/development/riccha_dev/HairProduct/setUserBase.php",
-                'data': JSON.stringify(dataToSend),
-                'processData': false,
-                'success': function (res, status) {
-                    var parse_data = JSON.parse(res);
-                    uniqueID = parse_data.data.id;
-                    sendOTP(parse_data.data.id, $('#phone').val());
-
-                }
-            })
+    };
+    $.post({
+        'url': "https://mojogamezone.com/development/riccha_dev/HairProduct/setUserBase.php",
+        'data': JSON.stringify(dataToSend),
+        'processData': false,
+        'success': function (res, status) {
+            var parse_data = JSON.parse(res);
+            if (parse_data.status === true) {
+                uniqueID = parse_data.data.id;
+                sendOTP(parse_data.data.id, $('#phone').val());
+            } else {
+                alert(parse_data.message);
+                togglePhoneField(false);
+                toggleSendOtpProgressBar(false);
+            }
+        },
+        complete: function () {
+            // $('#sendOTP').prop('disabled', false);
         }
-    }
+    })
+    // }
+    // }
 });
+
+
+/**
+ * Disable or enable phone input field and send OTP button
+ * @param {boolean} isDisable if true fields will be disabled else enable
+ */
+function togglePhoneField(isDisable) {
+    $("#phone, #sendOTP").prop('disabled', isDisable);
+}
+
+/**
+ * Disable or enable OTP input field and verify OTP button
+ * @param {boolean} isDisable if true fields will be disabled else enable
+ */
+function toggleOtpField(isDisable) {
+    $("#otp, #checkOTP").prop('disabled', isDisable);
+}
+
+
+/**
+ * Show or hide sending OTP progress bar
+ * @param {boolean} isShow If true progress bar will be visible else hidden
+ */
+function toggleSendOtpProgressBar(isShow) {
+    if (isShow === false) {
+        $('#opt-loader').hide();
+    } else {
+        $('#opt-loader').show();
+    }
+}
+
+/**
+ * Show or hide verify OTP progress bar
+ * @param {boolean} isShow If true progress bar will be visible else hidden
+ */
+function toggleVerifyOtpProgressBar(isShow) {
+    if (isShow === false) {
+        $('#verify-opt-loader').hide();
+    } else {
+        $('#verify-opt-loader').show();
+    }
+}
 
 function sendOTP(id, number) {
     var dataToSend = {
@@ -183,26 +255,33 @@ function sendOTP(id, number) {
             'processData': false,
             'success': function (res, status) {
                 var parse_data = JSON.parse(res);
-                if (parse_data.status == 'success') {
-                    $('#otp_send_msg').show();
-                    $('#otp,#checkOTP').prop('disabled', false)
-
+                if (parse_data.status === true) {
+                    // $('#otp_send_msg').show();
+                    // $('#otp,#checkOTP').prop('disabled', false);
+                    alert(parse_data.message);
+                    toggleOtpField(false);
                 } else {
-                    $('#otp_not_send_msg').show();
+                    alert(parse_data.message);
+                    toggleOtpField(true);
+
+
+                    // $('#otp,#checkOTP').prop('disabled', true);
                 }
             },
             'complete': function () {
-                setTimeout(() => {
+                togglePhoneField(false);
+                toggleSendOtpProgressBar(false);
+                /* setTimeout(() => {
 
                     $('#otp_send_msg,#otp_not_send_msg').hide();
-                }, 1000);
+                }, 3000); */
             }
-
         })
     }
 
 }
 $("#checkOTP").click(function () {
+
     var otp = $('#otp').val();
     var length = otp.length;
     if (length == 4) {
@@ -212,6 +291,9 @@ $("#checkOTP").click(function () {
                 'id': uniqueID,
                 'number': $("#phone").val()
             }
+            toggleOtpField(true);
+            toggleVerifyOtpProgressBar(true);
+
             $.post({
                 'url': "https://mojogamezone.com/development/riccha_dev/HairProduct/check_otp.php",
                 'data': JSON.stringify(dataToSend),
@@ -220,26 +302,33 @@ $("#checkOTP").click(function () {
                     var parse_data = JSON.parse(res);
 
                     if (parse_data.status == true) {
+
                         $('#after_otp').prop('disabled', false);
-                        $('#otp_verify_msg').show();
+                        /* $('#after_otp').prop('disabled', false);
+                        $('#otp_verify_msg').show(); */
+                        alert(parse_data.message);
 
                     } else {
+                        alert(parse_data.message);
+                        toggleOtpField(false);
 
-                        $('#otp_not_verify_msg').show();
+                        // $('#otp_not_verify_msg').show();
                     }
 
                 },
                 'complete': function () {
-                    setTimeout(() => {
+                    toggleVerifyOtpProgressBar(false);
+                    /* setTimeout(() => {
 
                         $("#otp_verify_msg,#otp_not_verify_msg").hide();
-                    }, 1000);
+                    }, 1000); */
                 }
             })
 
         }
     }
 });
+
 // function getEmail() {
 //     var email = $("#email").val()
 //     if (isEmail(email)) {
@@ -259,7 +348,6 @@ $("#checkOTP").click(function () {
 //             'processData': false,
 //             'success': function (data, status) {}
 //         })
-
 //     }
 // }
 
